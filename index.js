ae6d01ffefb11098c8947ccb8682a51ac7528b08
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-14 11:54:35 -0600

    Esqueleto principal de la app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Express server listening on ${port}`));
