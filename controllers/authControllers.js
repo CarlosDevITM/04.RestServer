d6856b234c6ca066aecd2ae08dc662df2d474712
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-18 16:59:35 -0600

    Se creó la validacion del login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,41 @@
 const { response } = require("express");
+const bcrypt = require("bcrypt");
+
+const Users = require("../models/user");
 
 const authController = {};
 
-authController.login = (req, res = response) => {
-  res.json({
-    msg: "Login Success",
-  });
+authController.login = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    //Verificar si el email existe
+    console.log(email);
+    const user = await Users.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        msg: "User incorrect",
+      });
+    }
+    //Verificar si el usuario está activo en a DB
+    if (user.status === false) {
+      return res.status(400).json({
+        msg: "User not authenticated status:false",
+      });
+    }
+    //Verificar la contraseña.
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Password incorrect",
+      });
+    }
+    res.json({
+      msg: "Login Success",
+    });
+  } catch (error) {
+    return res.status(500).send("El error es" + error.message);
+  }
 };
 
 module.exports = authController;
