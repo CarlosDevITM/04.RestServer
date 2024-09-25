8d50cf56ac23f3a33da4d8b40ab9d736738b588c
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-25 17:17:51 -0600

    Se agrego uuid para crear un nombre temporal de las imágenes y se creo una validación para el tipo de extensión de la imagen

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const { response } = require("express");
 
 const uploadFile = (req, res = response) => {
@@ -8,7 +9,26 @@ const uploadFile = (req, res = response) => {
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   const { myFile } = req.files;
-  const uploadPath = path.join(__dirname, "/uploads", myFile.name);
+  //Split value by dots
+  const cuttedName = myFile.name.split(".");
+
+  //extension of our file
+  const fileExtension = cuttedName[cuttedName.length - 1];
+  console.log(fileExtension);
+
+  //Validate the fileExtension
+  const allowedExtensions = ["png", "jpg", "jpeg", "gif"];
+
+  if (!allowedExtensions.includes(fileExtension)) {
+    return res.json({
+      error: `Please provide an allowed fileExtension of these list ${allowedExtensions}`,
+    });
+  }
+
+  //Temporal name
+  const temporalName = uuidv4() + "." + fileExtension;
+
+  const uploadPath = path.join(__dirname, "/uploads", temporalName);
 
   // Use the mv() method to place the file somewhere on your server
   myFile.mv(uploadPath, (err) => {
