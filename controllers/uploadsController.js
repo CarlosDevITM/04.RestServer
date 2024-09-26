6300ab578d8da34efd83f1cdf608140b90ebd78f
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-26 13:01:10 -0600

    Se creo una imagen a mostrar por defecto

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,6 +1,8 @@
 const { response } = require("express");
-const { uploadFileHelper } = require("../helpers/uploadFileHelper");
+const path = require("path");
+const fs = require("fs");
 
+const { uploadFileHelper } = require("../helpers/uploadFileHelper");
 const { User } = require("../models");
 
 const uploadFile = async (req, res = response) => {
@@ -45,6 +47,21 @@ const updateImage = async (req, res = response) => {
       `${collection} is not added yet, please add it`;
   }
 
+  //Clear previous files
+  if (model.image) {
+    console.log(model.image);
+    const imagePath = path.join(
+      __dirname,
+      "../uploads",
+      collection,
+      model.image
+    );
+    console.log(imagePath);
+    if (fs.existsSync(imagePath)) {
+      fs.unlinkSync(imagePath);
+    }
+  }
+
   const name = await uploadFileHelper(req.files, ["jpg", "png"], collection);
 
   model.image = name;
@@ -53,4 +70,47 @@ const updateImage = async (req, res = response) => {
   res.json(model);
 };
 
-module.exports = { uploadFile, updateImage };
+const getImage = async (req, res = response) => {
+  const { id, collection } = req.params;
+  let model;
+
+  switch (collection) {
+    case "users":
+      model = await User.findById(id);
+      if (!model)
+        return res.status(400).json({
+          msg: `It doesn´t exists an user with ${id}`,
+        });
+      break;
+
+    case "products":
+      model = await Products.findById(id);
+      if (!model)
+        return res.status(400).json({
+          msg: `It doesn´t exists a product with ${id}`,
+        });
+      break;
+
+    default:
+      `${collection} is not added yet, please add it`;
+  }
+
+  //Clear previous files
+  if (model.image) {
+    console.log(model.image);
+    const imagePath = path.join(
+      __dirname,
+      "../uploads",
+      collection,
+      model.image
+    );
+
+    if (fs.existsSync(imagePath)) {
+      return res.sendFile(imagePath);
+    }
+  }
+  const defaultImagePath = path.join(__dirname, "../img", "notFound.jpg");
+  res.sendFile(defaultImagePath);
+};
+
+module.exports = { uploadFile, updateImage, getImage };
