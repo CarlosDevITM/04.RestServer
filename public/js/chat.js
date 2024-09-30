48da9135229b0f41768d10ac3ca507ecbdf5a953
CarlosDevITM <carlos.ponce@devitm.com>
2024-09-30 16:11:27 -0600

    token validado llamandolo desde el front con fetch

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,32 @@
 console.log("Chat INDEX");
+//Validate if token user is correct
+let user = null;
+let socket = null;
+
+const validateJWT = async () => {
+  const token = localStorage.getItem("manualToken") || "";
+
+  if (token.length <= 10) {
+    throw new Error("The token is not a valid token");
+  }
+
+  try {
+    const resp = await fetch("http://localhost:8000/api/auth/", {
+      headers: { token: token },
+    });
+
+    const { user: userDB, token: tokenDB } = await resp.json();
+
+    console.log(userDB, tokenDB);
+  } catch (error) {
+    console.log({ error: error });
+  }
+};
+
+const main = async () => {
+  const result = validateJWT();
+};
+main();
+
 //Starting connection with server
-const socket = io();
+// const socket = io();
